76c006d66174661d9e341d22e16ecbf619bdb6c6
Romi <8305999@gmail.com>
2024-04-06 20:34:45 +0300

    Lesson_2

---
+++ src/src/Lesson_2_1.java
@@ -0,0 +1,14 @@
+/**
+ Lesson_2
+ *
+ */
+public class Lesson_2_1 {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
---
+++ src/src/Lesson_2_4.java
@@ -0,0 +1,19 @@
+/**
+ Lesson_2
+ *
+ */
+public class Lesson_2_4 {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+    public static void compareNumbers() {
+        int a = 47;
+        int b = 97;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+}
