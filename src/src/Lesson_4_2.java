14abc8a585677cecb56fb4e3e3bb2cf99b74e45b
Romi <8305999@gmail.com>
2024-04-22 00:21:08 +0300

    Lesson_4  (Доработка в связи с несоответствиеем задания в записи лекций с практ. заданием курса)

--- src/src/Lesson_4_2.java
+++ src/src/Lesson_4_2.java
@@ -8,17 +8,18 @@ public class Lesson_4_2 {
         Employee[] employees = new Employee[5];
 
         employees[0] = new Employee("Ivanov Ivan Ivanovich", "QA", "ivanovich@gmail.com", "+375291111111", 70000.0, 20);
-        employees[1] = new Employee("Chekhov Anton Pavlovich", "AQA", "pavlovich@gmail.com", "+375292222222", 80000.0, 30);
+        employees[1] = new Employee("Chekhov Anton Pavlovich", "AQA", "pavlovich@gmail.com", "+375292222222", 80000.0, 41);
         employees[2] = new Employee("Pushkin Alexander Sergeevich", "JAVA", "sidorov@gmail.com", "+375293333333", 90000.0, 35);
         employees[3] = new Employee("Tolstoy Lev Nicolaevich", "SA", "sergeevich@gmail.com", "+375294444444", 80000.0, 45);
         employees[4] = new Employee("Bulgakov Mikhail Afanasyevich ", "PM", "afanasyevich@gmail.com", "+375295555555", 70000.0, 18);
 
         for (int i = 0; i < employees.length; i++) {
-            employees[i].printInfo();
-            System.out.println();
+            if (employees[i].age > 40) {
+                employees[i].printInfo();
+                System.out.println();
+            }
         }
     }
-
     static class Employee {
         private String fullName;
         private String position;
