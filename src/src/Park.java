6babc1ef54007e46f61022d45ca5e87b23131305
Romi <8305999@gmail.com>
2024-05-05 13:07:21 +0300

    Lesson_4

--- src/src/Park.java
+++ src/src/Park.java
@@ -2,9 +2,7 @@
  * Lesson_4
  **/
 
-public class Lesson_4_3 {
-
-    public static class Park {
+public class Park {
 
         public static class Attractions {
 
@@ -18,11 +16,11 @@ public class Lesson_4_3 {
                 this.cost = cost;
             }
         }
-    }
+
 
     public static void main(String[] args) {
-        Park.Attractions attr1 = new Park.Attractions("Ладья", "09:00 - 22:00", 5.00);
-        Park.Attractions attr2 = new Park.Attractions("Колесо обозрения", "09:00 - 20:00", 10.00);
+        Attractions attr1 = new Attractions("Ладья", "09:00 - 22:00", 5.00);
+        Attractions attr2 = new Attractions("Колесо обозрения", "09:00 - 20:00", 10.00);
 
         System.out.println("Информация об аттракционе 1: " + attr1.information + ", Время работы: " + attr1.worktime + ", Стоимость руб: " + attr1.cost);
         System.out.println("Информация об аттракционе 2: " + attr2.information + ", Время работы: " + attr2.worktime + ", Стоимость руб: " + attr2.cost);
